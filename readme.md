536888107959a295750cc417f898a8ce1994b9f8
Nicolas Crausaz <n.crausaz99@gmail.com>
2023-06-15 18:47:57 +0200

    Readme

--- readme.md
+++ readme.md
@@ -8,22 +8,102 @@ Dans les itérations précèdentes de Bot-tender, on supposait une commande prê
 de ce labo est d’intégrer une gestion de commandes asynchrones en utilisant la classe Future de
 l’API Scala.
 
-# Choix architecturaux
+# Choix architecturaux et d’implémentation
 
-TODO: En plus du code source, il faut rendre un fichier README contenant une explication de vos choix
-architecturaux et d’implémentation (pas plus que 2 pages).
+## Préparation des produits
 
-N’utilisez pas Future.onComplete ou Await.
+Pour chaque marque de produit, nous avons spécifié un temps de préparation. Nous l'avons fait en modifiant la structure de données qui stocke les produits sous la forme d'un `Map[String, (Double, Duration)]`. Pour appliquer les temps de préparation, nous avons ajouté une méthode `startPreparation` au `ProductService` chargée de préparer une produit de manière asynchrone (future), avec une probabilité 0.5 de réussite.
 
+## AnalyzerService
 
-Pour gérer les réponses immédiates et les réponses asynchrones, nous avons décidé de retourner un tuple dans la fonction reply (réponse immédiate, Réponse future)
+Le bot recevant une commande est capable de récéptionner une commande et de la préparer dans une Future. Afin de répondre directement au client, le bot renvoie un tuple contenant la réponse immédiate et la réponse future.
 
-Nous avons attribué un temps de préparation à tous les produits. Tout les produits on également 0.5 de probabilité d'être disponible.
+Comme indiqué au point précédent, une commande peut échouer, ainsi lors de commande de plusieurs produits (n fois le même ou plusieurs produits différents), il est possible que certains produits ne soient pas disponibles. Pour gérer ce cas, nous avons ajouté un enum `PreparationStatus` qui permet de gérer les différents résultats de la préparation des commandes (Success, Partial, Failure).
 
-Expliquer le fonctionnement de la préparation des commandes
+Grâce à cela, nous pouvons gérer les échecs et les commandes partiellement préparées, notamment pour les commandes avec plusieurs produits et répondre au client en conséquence.
 
-Nous avons ajouté un enum qui permet de gérer les différents résultats de la préparation des commandes (Success, Partial, Failure).
+Voici quelques exemples de réponses possibles avec la commande d'un produit (quantité n):
 
-Cela nous permet de gèrer les échecs et les commandes partiellement préparées, notamment pour les commandes avec plusieurs produits:
+![Exemple de réponse](./docs/exemple_1prod.png)
 
-TODO: CAPTURE ICI
+Chaque produit est préparé de manière séquentielle (un après l'autre)
+
+La commande de plusieurs produits en quantités différentes (ET) à été implementée grâce à l'utilisation de `foldLeft`:
+
+```scala
+val preparations = (1 to quantity).foldLeft(
+  Future.successful(List.empty[PreparationStatus])
+) { (previousFuture, _) =>
+  previousFuture.flatMap { list =>
+    productSvc
+      .startPreparation(name, brand)
+      .map(_ => PreparationStatus.Success)
+      .recover(_ => PreparationStatus.Failure)
+      .map(result => list :+ result)
+  }
+}
+```
+
+Le nombre de succès est ensuite compté et comparé au nombre de produits commandés pour déterminer le résultat de la commande (Success, Partial, Failure).
+
+Pour la préparation de plusieurs produits en plusieurs quantités (ET), chaque type de produit est préparé en parallèle, puis les n produits sont préparés séquentiellement. Le résultat est ensuite calculé de la même manière que pour la commande d'un produit:
+
+```scala
+val leftPreparation = prepare(left)
+val rightPreparation = prepare(right)
+
+for {
+  (leftResult, leftStatus) <- leftPreparation
+  (rightResult, rightStatus) <- rightPreparation
+} yield {
+  (leftStatus, rightStatus) match {
+    // Both success
+    case (PreparationStatus.Success, PreparationStatus.Success) =>
+      (Some(And(left, right)), PreparationStatus.Success)
+
+    // Both failed
+    case (PreparationStatus.Failure, PreparationStatus.Failure) =>
+      (None, PreparationStatus.Failure)
+
+    // Both partial
+    case (PreparationStatus.Partial, PreparationStatus.Partial) =>
+      (
+        Some(And(leftResult.get, rightResult.get)),
+        PreparationStatus.Partial
+      )
+
+    // One failed, return the one that didn't
+    case _ =>
+      (leftResult.orElse(rightResult), PreparationStatus.Partial)
+  }
+```
+
+Voici un exemple de réponse possible avec la commande de plusieurs produits:
+
+![Exemple de réponse](./docs/exemple_2prod.png)
+
+Une fois la commande préparée, le bot envoie un message au client pour l'informer que sa commande est prête (ou échec).
+Nous pouvons gérer facilement les trois status possibles grâce à notre enum:
+
+```scala
+val baseOrder = inner(products)._1
+val order = prepare(products)
+  .map(task => {
+    task._2 match
+      case PreparationStatus.Success => {
+        accountSvc.purchase(user, orderPrice)
+        s"La commande de ${baseOrder} est prête. Cela coute $orderPrice.-"
+      }
+
+      case PreparationStatus.Partial => {
+        val ajustedPrice = computePrice(task._1.get)
+        val adjustedOrder = inner(task._1.get)._1
+        accountSvc.purchase(user, ajustedPrice)
+        s"La commande de ${baseOrder} est partiellement prête. Voici ${adjustedOrder}. Cela coute $ajustedPrice.-"
+      }
+
+      case PreparationStatus.Failure => {
+        s"La commande de ${baseOrder} ne peut pas être délivrée."
+      }
+  })
+```
--- src/test/scala/AnalyzerServiceSuite.scala
+++ src/test/scala/AnalyzerServiceSuite.scala
@@ -28,7 +28,7 @@ class AnalyzerServiceSuite
       val thirstyParser = new Parser(thirstyTokens)
       val thirstyExpr = thirstyParser.parsePhrases()
       val thirstyResult = analyzerSvc.reply(session)(thirstyExpr)
-      thirstyResult should include("Eh bien, la chance est de votre côté car nous offrons les meilleures bières de la région !")
+      thirstyResult._1 should include("Eh bien, la chance est de votre côté car nous offrons les meilleures bières de la région !")
     }
 
     "handle Auth input" in {
@@ -37,7 +37,7 @@ class AnalyzerServiceSuite
       val authParser = new Parser(authTokens)
       val authExpr = authParser.parsePhrases()
       val authResult = analyzerSvc.reply(session)(authExpr)
-      authResult should include("Bonjour, michel !")
+      authResult._1 should include("Bonjour, michel !")
     }
 
     "handle Price input" in {
@@ -46,7 +46,7 @@ class AnalyzerServiceSuite
       val priceParser = new Parser(priceTokens)
       val priceExpr = priceParser.parsePhrases()
       val priceResult = analyzerSvc.reply(session)(priceExpr)
-      priceResult should include("Cela coûte CHF 3.0.")
+      priceResult._1 should include("Cela coûte CHF 3.0.")
     }
 
     "handle Solde input" in {
@@ -55,29 +55,7 @@ class AnalyzerServiceSuite
       val soldeParser = new Parser(soldeTokens)
       val soldeExpr = soldeParser.parsePhrases()
       val soldeResult = analyzerSvc.reply(session)(soldeExpr)
-      soldeResult should include("solde est de CHF 30.0.")
-    }
-
-    "handle Command Biers input" in {
-      val commandInput = "Je veux commander 2 bières PunkIPAs et 1 bière Ténébreuse."
-      val commandTokens = tokenizerSvc.tokenize(commandInput)
-      val commandParser = new Parser(commandTokens)
-      val commandExpr = commandParser.parsePhrases()
-      val commandResult = analyzerSvc.reply(session)(commandExpr)
-      commandResult should include("Voici donc 2 biere punkipa et 1 biere tenebreuse !")
-      commandResult should include("Cela coûte CHF 10.0")
-      commandResult should include("solde est de CHF 20.0")
-    }
-
-    "handle Command Croissant input" in {
-      val commandInput = "Je voudrais commander 1 croissant."
-      val commandTokens = tokenizerSvc.tokenize(commandInput)
-      val commandParser = new Parser(commandTokens)
-      val commandExpr = commandParser.parsePhrases()
-      val commandResult = analyzerSvc.reply(session)(commandExpr)
-      commandResult should include("Voici donc 1 croissant maison !")
-      commandResult should include("Cela coûte CHF 2.0")
-      commandResult should include("solde est de CHF 18.0")
+      soldeResult._1 should include("solde est de CHF 30.0.")
     }
 
     "handle another Auth input" in {
@@ -86,7 +64,7 @@ class AnalyzerServiceSuite
       val authParser = new Parser(authTokens)
       val authExpr = authParser.parsePhrases()
       val authResult = analyzerSvc.reply(session)(authExpr)
-      authResult should include("Bonjour, bobby !")
+      authResult._1 should include("Bonjour, bobby !")
     }
 
     "handle Hungry input" in {
@@ -95,50 +73,7 @@ class AnalyzerServiceSuite
       val hungryParser = new Parser(hungryTokens)
       val hungryExpr = hungryParser.parsePhrases()
       val hungryResult = analyzerSvc.reply(session)(hungryExpr)
-      hungryResult should include("Pas de soucis ! Nous pouvons vous offrir des croissants faits maisons !")
-    }
-
-    "handle another Command input" in {
-      val commandInput = "Je veux commander 2 croissants cailler."
-      val commandTokens = tokenizerSvc.tokenize(commandInput)
-      val commandParser = new Parser(commandTokens)
-      val commandExpr = commandParser.parsePhrases()
-      val commandResult = analyzerSvc.reply(session)(commandExpr)
-      commandResult should include("Voici donc 2 croissant cailler !")
-      commandResult should include("Cela coûte CHF 4.0")
-      commandResult should include("solde est de CHF 26.0")
-    }
-
-    "handle another Solde input" in {
-      val soldeInput = "J'aimerais connaitre mon solde."
-      val soldeTokens = tokenizerSvc.tokenize(soldeInput)
-      val soldeParser = new Parser(soldeTokens)
-      val soldeExpr = soldeParser.parsePhrases()
-      val soldeResult = analyzerSvc.reply(session)(soldeExpr)
-      soldeResult should include("solde est de CHF 26.0.")
-    }
-
-    "switch again to Michel and command 18 Farmers" in {
-      // Switch to Michel
-      analyzerSvc.reply(session)(new Parser(tokenizerSvc.tokenize("Je suis _Michel.")).parsePhrases())
-
-      val commandInput = "J'aimerais commander 18 bières Farmer."
-      val commandTokens = tokenizerSvc.tokenize(commandInput)
-      val commandParser = new Parser(commandTokens)
-      val commandExpr = commandParser.parsePhrases()
-      val commandResult = analyzerSvc.reply(session)(commandExpr)
-      commandResult should include("Voici donc 18 biere farmer !")
-      commandResult should include("Cela coûte CHF 18.0")
-      commandResult should include("solde est de CHF 0.0")
-    }
-
-    "handle command with no money" in {
-      val commandInput = "J'aimerais commander 1 bière Farmer."
-      val commandTokens = tokenizerSvc.tokenize(commandInput)
-      val commandParser = new Parser(commandTokens)
-      val commandExpr = commandParser.parsePhrases()
-      val commandResult = analyzerSvc.reply(session)(commandExpr)
-      commandResult should include("Vous n'avez pas assez d'argent pour effectuer cette commande.")
+      hungryResult._1 should include("Pas de soucis ! Nous pouvons vous offrir des croissants faits maisons !")
     }
 
     "handle price with associability" in {
@@ -147,7 +82,7 @@ class AnalyzerServiceSuite
       val priceParser = new Parser(priceTokens)
       val priceExpr = priceParser.parsePhrases()
       val priceResult = analyzerSvc.reply(session)(priceExpr)
-      priceResult should include("Cela coûte CHF 2.0.")
+      priceResult._1 should include("Cela coûte CHF 2.0.")
     }
   }
 }
